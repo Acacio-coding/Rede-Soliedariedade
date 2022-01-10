2e45e391b96e782cbf99177c6cf0661668346270
Acacio.coding <acacio.coding@gmail.com>
2022-01-10 17:21:06 -0300

    Layout creation and static files structure

--- gestao_de_doacoes/urls.py
+++ gestao_de_doacoes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from gestao_de_doacoes import views
 
 urlpatterns = [
-  
+    path('', views.index, name='index'),
 ]
--- gestao_de_doacoes/views.py
+++ gestao_de_doacoes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from gestao_de_doacoes.models import Familia, IntegranteFamiliar, Entidade, Representante, Item, Doacao, ItensDoacao
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
